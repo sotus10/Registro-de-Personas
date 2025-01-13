55d98e954441c6a02e007abb45992639dbe65b79
Juan Diego Soto <hornodelaabue@gmail.com>
2025-01-13 10:24:56 -0500

    Update

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -1,4 +1,5 @@
 //Importamos los componentes al documento principal App.js
+import Equipo from './componentes/Equipo';
 import {useState} from 'react';
 import './App.css';
 import Header from './componentes/Header/Header';
@@ -7,7 +8,7 @@ import MiOrg from './componentes/MiOrg';
 
 function App() {
 
-  const [mostrarFormulario, actualizarMostrar] = useState(true);
+  const [mostrarFormulario, actualizarMostrar] = useState(false);
 
   //Ternario --> condición? si es verdadero : si es falso
 
@@ -15,12 +16,59 @@ function App() {
     actualizarMostrar(!mostrarFormulario)
   }
 
+  //Lista de equipos
+  const equipos = [
+    {
+      titulo: "Programación",
+      colorPrimario: "#57c278",
+      colorSecundario: "#d9f7e9"
+    },
+    {
+      titulo: "Front End",
+      colorPrimario: "#82cffa",
+      colorSecundario: "#e8f8ff"
+    },
+    {
+      titulo: "Data Science",
+      colorPrimario: "#a6d157",
+      colorSecundario: "#f0f8e2"
+    },
+    {
+      titulo: "Devops",
+      colorPrimario: "#e06b69",
+      colorSecundario: "#fde7e8"
+    },
+    {
+      titulo: "UX y Diseño",
+      colorPrimario: "#db6ebf",
+      colorSecundario: "#fae9f5"
+    },
+    {
+      titulo: "Móvil",
+      colorPrimario: "#ffba05",
+      colorSecundario: "#fff5d9"
+    },
+    {
+      titulo: "Innovación y Gestión",
+      colorPrimario: "#ff8a29",
+      colorSecundario: "#ffeedf"
+    }
+]
+
   return (
     <div >
       <Header />
       { /*mostrarFormulario === true ? <Formulario /> : <></> */}
       { mostrarFormulario && <Formulario /> }
       <MiOrg  cambiarMostrar={cambiarMostrar}/>
+
+      {
+        equipos.map((equipo) => {
+          console.log("Equipo:", equipo);
+          return <Equipo /> 
+        } )
+      }
+
     </div>
   );
 }
